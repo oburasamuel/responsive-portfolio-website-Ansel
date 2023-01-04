6754e01281816cf389768afcb623afb51ab1a2c6
Samuel Obura <oburasamuel4@gmail.com>
2023-01-04 20:27:59 +0300

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,66 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+const scrollHeader = () =>{
+  const header = document.getElementById('header')
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  this.scrollY >= 50 ? header.classList.add('scroll-header') 
+                     : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== SERVICES MODAL ===============*/
+const modalViews = document.querySelectorAll('.services__modal'),
+      modalBtns = document.querySelectorAll('.services__button'),
+      modalClose = document.querySelectorAll('.services__modal-close')
 
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((mb, i) =>{
+    mb.addEventListener('click', () =>{
+        modal(i)
+    })
+})
+
+modalClose.forEach((mc) =>{
+  mc.addEventListener('click', () =>{
+      modalViews.forEach((mv) =>{
+          mv.classList.remove('active-modal')
+      })
+  })
+})
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
-
+let mixerPortfolio = mixitup('.work__container', {
+    selectors: {
+        target: '.work__card'
+    },
+    animation: {
+      duration: 300
+    }
+});
 
 /* Link active work */ 
+const linkWork = document.querySelectorAll('.work__item')
 
+function activeWork(){
+    linkWork.forEach(l=> l.classList.remove('active-work'))
+    this.classList.add('active-work')
+}
+
+linkWork.forEach(l=> l.addEventListener('click', activeWork))
 
 /*=============== SWIPER TESTIMONIAL ===============*/
-
+let swiperTestimonial = new Swiper(".testimonial__container", {
+    spaceBetween: 24,
+    loop: true,
+    grabCursor: true,
+    
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+})
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
